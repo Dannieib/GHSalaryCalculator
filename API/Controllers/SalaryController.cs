2f8f9be2a881f7e698f00ab9fef7bad59f758e22
Daniel Ibanga <danielibanga4321@outlook.com>
2021-11-20 22:11:29 +0100

    Add project files.

---
+++ API/Controllers/SalaryController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using GHSalaryCalculator.Service;
+using GHSalaryCalculator.Service.Dto;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GHSalaryCalculator.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SalaryController : ControllerBase
+    {
+
+        private readonly ILogger<SalaryController> _logger;
+        private readonly IServiceHandler _serviceHandler;
+
+        public SalaryController(ILogger<SalaryController> logger, IServiceHandler serviceHandler)
+        {
+            _logger = logger;
+            _serviceHandler = serviceHandler;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(ProcessGrossSalaryDto),200)]
+        [ProducesResponseType(typeof(ProcessGrossSalaryDto),500)]
+        public async Task<IActionResult> ProcessGrossSalary(string userName, decimal grossSalary)
+        {
+            try
+            {
+                var resp = await _serviceHandler.ProcessGrossSalary(userName, grossSalary);
+                return Ok(resp);
+            }
+            catch (Exception e)
+            {
+                _logger.LogCritical(e.Message,e);
+                throw;
+            }
+        }
+    }
+}
